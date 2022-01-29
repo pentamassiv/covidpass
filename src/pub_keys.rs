e48c7fe552d67553d953e014bf941035fa2209bf
pentamassiv <pentamassiv@posteo.de>
2022-01-29 01:51:23 +0100

    Use pub_keys from downloaded file

--- src/pub_keys.rs
+++ src/pub_keys.rs
@@ -1,6 +1,7 @@
 use curl::easy::Easy;
 use std::fs::File;
-use std::io::Write;
+use std::io::{self, BufRead, Write};
+use std::path::Path;
 
 pub fn fetch_public_keys() {
     let url = "https://de.dscg.ubirch.com/trustList/DSC/";
@@ -50,6 +51,29 @@ fn fetch_url(url: &str) -> String {
 
     // Convert it to `String`
     let body = String::from_utf8(data).expect("body is not valid UTF8!");
-    println!("{}", body);
     body
+}
+
+pub fn read_file(filename: &str) -> Vec<String> {
+    let mut line_vec = Vec::new();
+    // File hosts must exist in current path before this produces output
+    if let Ok(lines) = read_lines(filename) {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines {
+            if let Ok(ip) = line {
+                line_vec.push(ip);
+            }
+        }
+    }
+    line_vec
+}
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
