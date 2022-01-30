5cb903b747f25d87cc0caea317cc87e5bef4ed94
pentamassiv <pentamassiv@posteo.de>
2022-01-30 01:26:41 +0100

    Refactoring

--- src/ui/start_page.rs
+++ src/ui/start_page.rs
@@ -1,5 +1,6 @@
 use super::*;
 
+#[derive(Clone)]
 pub struct StartPage {
     content: gtk::Box,
     leaflet: adw::Leaflet,
@@ -47,22 +48,13 @@ impl StartPage {
     }
 
     fn connect(&self) {
-        let leaflet = self.leaflet.clone();
-        let toast_overlay = self.toast_overlay.clone();
         let button = self.button.clone();
         let file_chooser = self.file_chooser.clone();
-        let view_stack = self.view_stack.clone();
-
+        let self_clone = self.clone();
         // Connect to response of the file chooser
         file_chooser.connect_response(move |file_chooser, response| {
-            handle_file_chooser_response(
-                file_chooser,
-                response,
-                &leaflet,
-                &toast_overlay,
-                &view_stack,
-            );
             file_chooser.hide();
+            self_clone.handle_file_chooser_response(response);
         });
 
         // Connect to "clicked" signal of `button`
@@ -74,78 +66,68 @@ impl StartPage {
     pub fn content(&self) -> &gtk::Box {
         &self.content
     }
-}
 
-fn handle_file_chooser_response(
-    file_chooser: &gtk::FileChooserNative,
-    response: gtk::ResponseType,
-    leaflet: &adw::Leaflet,
-    toast_overlay: &adw::ToastOverlay,
-    view_stack: &adw::ViewStack,
-) {
-    if response == gtk::ResponseType::Accept {
-        if let Some(file) = file_chooser.file() {
-            if let Some(path) = file.path() {
-                if let Ok(((first_name, surname, full_name), cert)) = cert::load_certificate(&path)
-                {
-                    add_qr_png(&first_name, &full_name, &leaflet, view_stack);
-                    throw_toast(ToastType::Success(first_name), &toast_overlay);
+    fn handle_file_chooser_response(&self, response: gtk::ResponseType) {
+        if response == gtk::ResponseType::Accept {
+            if let Some(file) = self.file_chooser.file() {
+                if let Some(path) = file.path() {
+                    if let Ok(((first_name, surname, full_name), cert)) =
+                        cert::load_certificate(&path)
+                    {
+                        self.add_qr_png(&first_name, &full_name);
+                        throw_toast(ToastType::Success(first_name), &self.toast_overlay);
+                    } else {
+                        throw_toast(ToastType::CertInvalid, &self.toast_overlay);
+                    }
                 } else {
-                    throw_toast(ToastType::CertInvalid, &toast_overlay);
+                    throw_toast(ToastType::FileInvalid, &self.toast_overlay);
                 }
             } else {
-                throw_toast(ToastType::FileInvalid, &toast_overlay);
+                throw_toast(ToastType::FileInvalid, &self.toast_overlay);
             }
         } else {
-            throw_toast(ToastType::FileInvalid, &toast_overlay);
+            throw_toast(ToastType::Aborted, &self.toast_overlay);
         }
-    } else {
-        throw_toast(ToastType::Aborted, &toast_overlay);
     }
-}
 
-fn add_qr_png(
-    givenname: &str,
-    full_name: &str,
-    leaflet: &adw::Leaflet,
-    view_stack: &adw::ViewStack,
-) {
-    let qr_png = Image::from_file("/tmp/qrcode.png");
-    qr_png.set_vexpand(true);
-    qr_png.set_hexpand(true);
-    qr_png.set_margin_top(4);
-    qr_png.set_margin_bottom(4);
-    qr_png.set_margin_start(4);
-    qr_png.set_margin_end(4);
+    fn add_qr_png(&self, givenname: &str, full_name: &str) {
+        let qr_png = Image::from_file("/tmp/qrcode.png");
+        qr_png.set_vexpand(true);
+        qr_png.set_hexpand(true);
+        qr_png.set_margin_top(4);
+        qr_png.set_margin_bottom(4);
+        qr_png.set_margin_start(4);
+        qr_png.set_margin_end(4);
 
-    let label_short_name = gtk::Label::new(Some(&givenname));
-    let label_full_name = gtk::Label::new(Some(&full_name));
+        let label_short_name = gtk::Label::new(Some(&givenname));
+        let label_full_name = gtk::Label::new(Some(&full_name));
 
-    label_short_name.set_hexpand(true);
-    label_full_name.set_hexpand(true);
+        label_short_name.set_hexpand(true);
+        label_full_name.set_hexpand(true);
 
-    let squeezer = adw::Squeezer::new();
-    squeezer.add(&label_full_name);
-    squeezer.add(&label_short_name);
+        let squeezer = adw::Squeezer::new();
+        squeezer.add(&label_full_name);
+        squeezer.add(&label_short_name);
 
-    let vbox_cert = gtk::Box::new(Orientation::Vertical, 0);
-    vbox_cert.append(&qr_png);
-    vbox_cert.append(&squeezer);
+        let vbox_cert = gtk::Box::new(Orientation::Vertical, 0);
+        vbox_cert.append(&qr_png);
+        vbox_cert.append(&squeezer);
 
-    let button_qr = gtk::Button::new();
-    button_qr.set_child(Some(&vbox_cert));
+        let button_qr = gtk::Button::new();
+        button_qr.set_child(Some(&vbox_cert));
 
-    leaflet.append(&button_qr);
+        self.leaflet.append(&button_qr);
 
-    // Connect to "clicked" signal of `button`
-    let view_stack = view_stack.clone();
-    let givenname_clone: String = givenname.into();
-    button_qr.connect_clicked(move |_| {
-        println!("Pushed qr for {}", givenname_clone);
-        println!(
-            "Currently visible: {}",
-            view_stack.visible_child_name().unwrap()
-        );
-        view_stack.set_visible_child_name("detail_page");
-    });
+        // Connect to "clicked" signal of `button`
+        let view_stack = self.view_stack.clone();
+        let givenname_clone: String = givenname.into();
+        button_qr.connect_clicked(move |_| {
+            println!("Pushed qr for {}", givenname_clone);
+            println!(
+                "Currently visible: {}",
+                view_stack.visible_child_name().unwrap()
+            );
+            view_stack.set_visible_child_name("detail_page");
+        });
+    }
 }
